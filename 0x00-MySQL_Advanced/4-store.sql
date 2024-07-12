e6d25c89f96739d26f36095a6f379a59011391af
Your Name <you@example.com>
2024-07-12 05:15:58 -0700

    Tsk 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,20 +1,4 @@
--- 4-init.sql
-DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS orders;
-
-CREATE TABLE IF NOT EXISTS items (
-    name VARCHAR(255) NOT NULL,
-    quantity int NOT NULL DEFAULT 10
-);
-
-CREATE TABLE IF NOT EXISTS orders (
-    item_name VARCHAR(255) NOT NULL,
-    number int NOT NULL
-);
-
-INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
-
--- 4-store.sql
+-- Show and add orders
 DELIMITER //
 
 CREATE TRIGGER decrease_quantity_after_order
@@ -27,17 +11,3 @@ BEGIN
 END//
 
 DELIMITER ;
-
--- 4-main.sql
--- Show and add orders
-SELECT * FROM items;
-SELECT * FROM orders;
-
-INSERT INTO orders (item_name, number) VALUES ('apple', 1);
-INSERT INTO orders (item_name, number) VALUES ('apple', 3);
-INSERT INTO orders (item_name, number) VALUES ('pear', 2);
-
-SELECT "--";
-
-SELECT * FROM items;
-SELECT * FROM orders;
