4d6511ab90db8afe1a154f91711db6cc5a9c30e9
Your Name <you@example.com>
2024-07-12 05:42:29 -0700

    Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,16 +5,13 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
-    
     -- Calculate the average score for the given user_id
     SELECT AVG(score) INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
-    
     -- Update the average_score in the users table for the given user_id
     UPDATE users
     SET average_score = avg_score
     WHERE id = user_id;
 END //
-
 DELIMITER ;
