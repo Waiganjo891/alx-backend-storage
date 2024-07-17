f584ba7121dfed9a4a93d97f087341a27cfbbbcb
Your Name <you@example.com>
2024-07-17 15:44:02 -0700

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ Cache module
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -32,3 +32,57 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self, key: str, fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieves data from Redis and applies an optional conversion function.
+        Args:
+            key (str): The key under which the data is stored.
+            fn (Optional[Callable]): A callable to convert the data.
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data or
+            None if the key does not exist.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieves a string from Redis.
+        Args:
+            key (str): The key under which the data is stored.
+        Returns:
+            Optional[str]: The retrieved string or None if
+            the key does not exist.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieves an integer from Redis.
+        Args:
+            key (str): The key under which the data is stored.
+        Returns:
+            Optional[int]: The retrieved integer or None if the
+            key does not exist.
+        """
+        return self.get(key, fn=int)
+
+
+if __name__ == "__main__":
+    cache = Cache()
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
