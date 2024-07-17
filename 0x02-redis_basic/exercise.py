019056f6d49d61110fff20aacb387585d48ea94d
Your Name <you@example.com>
2024-07-17 16:39:28 -0700

    Task 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """
 Cache module
 """
+
 import redis
 import uuid
 from typing import Union, Callable, Optional
@@ -11,8 +12,10 @@ from functools import wraps
 def count_calls(method: Callable) -> Callable:
     """
     Decorator to count the number of calls to a method.
+
     Args:
         method (Callable): The method to be decorated.
+
     Returns:
         Callable: The wrapped method with call counting.
     """
@@ -28,8 +31,10 @@ def call_history(method: Callable) -> Callable:
     """
     Decorator to store the history of inputs and
     outputs for a particular function.
+
     Args:
         method (Callable): The method to be decorated.
+
     Returns:
         Callable: The wrapped method with call history logging.
     """
@@ -47,6 +52,7 @@ def call_history(method: Callable) -> Callable:
 class Cache:
     """
     A Cache class to interact with a Redis database.
+
     Attributes:
         _redis (redis.Redis): Instance of Redis client.
     """
@@ -57,11 +63,15 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores data in Redis with a randomly generated key.
+
         Args:
             data (Union[str, bytes, int, float]): Data to store in Redis.
+
         Returns:
             str: The generated key under which the data is stored.
         """
@@ -74,9 +84,11 @@ class Cache:
             ) -> Union[str, bytes, int, float, None]:
         """
         Retrieves data from Redis and applies an optional conversion function.
+
         Args:
             key (str): The key under which the data is stored.
             fn (Optional[Callable]): A callable to convert the data.
+
         Returns:
             Union[str, bytes, int, float, None]: The retrieved data or
             None if the key does not exist.
@@ -91,8 +103,10 @@ class Cache:
     def get_str(self, key: str) -> Optional[str]:
         """
         Retrieves a string from Redis.
+
         Args:
             key (str): The key under which the data is stored.
+
         Returns:
             Optional[str]: The retrieved string or None if
             the key does not exist.
@@ -102,8 +116,10 @@ class Cache:
     def get_int(self, key: str) -> Optional[int]:
         """
         Retrieves an integer from Redis.
+
         Args:
             key (str): The key under which the data is stored.
+
         Returns:
             Optional[int]: The retrieved integer or None if the
             key does not exist.
@@ -111,17 +127,28 @@ class Cache:
         return self.get(key, fn=int)
 
 
+def replay(method: Callable):
+    """
+    Displays the history of calls of a particular function.
+
+    Args:
+        method (Callable): The function to replay the history of calls.
+    """
+    cache = method.__self__
+    inputs_key = f"{method.__qualname__}:inputs"
+    outputs_key = f"{method.__qualname__}:outputs"
+    inputs = cache._redis.lrange(inputs_key, 0, -1)
+    outputs = cache._redis.lrange(outputs_key, 0, -1)
+    print(f"{method.__qualname__} was called {len(inputs)} times:")
+    for inp, out in zip(inputs, outputs):
+        inp_str = inp.decode('utf-8')
+        out_str = out.decode('utf-8')
+        print(f"{method.__qualname__}(*{inp_str}) -> {out_str}")
+
+
 if __name__ == "__main__":
     cache = Cache()
-    s1 = cache.store("first")
-    print(s1)
-    s2 = cache.store("second")
-    print(s2)
-    s3 = cache.store("third")
-    print(s3)
-    inputs = cache._redis.lrange(
-                f"{cache.store.__qualname__}:inputs", 0, -1)
-    outputs = cache._redis.lrange(
-                f"{cache.store.__qualname__}:outputs", 0, -1)
-    print(f"inputs: {inputs}")
-    print(f"outputs: {outputs}")
+    cache.store("foo")
+    cache.store("bar")
+    cache.store(42)
+    replay(cache.store)
